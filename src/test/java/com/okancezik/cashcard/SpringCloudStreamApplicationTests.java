b3cb62eed3589ec2cf085cc3cdbad2d857320428
Okan Çezik <okan.cezik@i2i-systems.com>
2025-01-29 14:27:44 +0300

    init

---
+++ src/test/java/com/okancezik/cashcard/SpringCloudStreamApplicationTests.java
@@ -0,0 +1,11 @@
+package com.okancezik.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
